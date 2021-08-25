b98a4cce92ee902c50b8fc59618fdad81596925a
Alex Kalkatos <alexcsts@gmail.com>
2021-08-25 00:03:28 -0300

    Additional settings for punch move

--- BarFight/Assets/Scripts/HitMove.cs
+++ BarFight/Assets/Scripts/HitMove.cs
@@ -9,20 +9,32 @@ namespace BarFight
         public CharacterStance AllowedHitStances;
 		public ContactFilter2D Filter;
 		public float maxDepth;
+		public GameObject effect;
 
 		private RaycastHit2D[] hits = new RaycastHit2D[10];
 
         public override void Execute ()
 		{
+			//Visual
+			effect.SetActive(true);
+			Invoke("Deactivate", Time);
+			//Hit
             int hitAmount = HitBox.Cast(Vector2.zero, Filter, hits, 0.1f);
             if (hitAmount > 0)
 			{
 				for (int i = 0; i < hitAmount; i++)
 				{
 					if (Mathf.Abs(hits[i].transform.position.z - transform.position.z) < maxDepth)
+					{
 						Debug.Log("Hit " + hits[i].collider.name);
+					}
 				}
 			}
+		}
+
+		private void Deactivate ()
+		{
+			effect.SetActive(false);
 		}
 	}
 }
