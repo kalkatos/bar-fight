a29e291a303ad15cf838ca42ca083ec20d2a5099
bruno de miranda cintra <brunocintra.777@gmail.com>
2021-08-28 15:15:14 -0300

    Included animations on main scene and integrating with move controller

--- BarFight/Assets/Scripts/MovingController.cs
+++ BarFight/Assets/Scripts/MovingController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,11 +15,13 @@ namespace BarFight
 		private bool canMove;
 		private Rigidbody2D rb;
 		private SortingGroup sortingGroup;
+        private Animator animator;
 
-		private void Awake ()
+        private void Awake ()
 		{
 			rb = GetComponent<Rigidbody2D>();
 			sortingGroup = GetComponent<SortingGroup>();
+			animator = GetComponentInChildren<Animator>();
 		}
 
 		private void Start ()
@@ -38,9 +41,33 @@ namespace BarFight
 				rb.velocity = MoveDirection * Speed;
 			else
 				rb.velocity = Vector2.zero;
+
+			updateAnimation(rb.velocity);
+			faceWalkingDirection(rb.velocity);
 		}
 
-		private void SetZPosition ()
+        private void faceWalkingDirection(Vector2 velocity)
+        {
+			Vector3 charecterScale = transform.localScale;
+			if (velocity.x < 0)
+			{
+				charecterScale.x = -1;
+			}
+			if (velocity.x > 0)
+			{
+				charecterScale.x = 1;
+			}
+			transform.localScale = charecterScale;
+		}
+
+        private void updateAnimation(Vector2 velocity)
+        {
+			if (animator == null) return;
+
+			animator.SetFloat("speed", Mathf.Abs(velocity.x) + Mathf.Abs(velocity.y));
+        }
+
+        private void SetZPosition ()
 		{
 			Vector3 pos = transform.position;
 			pos.z = pos.y;
